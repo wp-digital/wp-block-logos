3fa57006e8e8c062adf4122fcddc38086bfd4636
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-07-04 23:53:21 +0300

    fix editor margins etc.

--- blocks/logo/build/index.asset.php
+++ blocks/logo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '7375fadcdfae689e6685');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'c8b10264f5a3277b51ed');
--- blocks/logos/build/index.asset.php
+++ blocks/logos/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'a1a4415dec2367bedf33');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e4428cded2b950b6fcb6');
